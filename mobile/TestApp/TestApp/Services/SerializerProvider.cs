c8fddee222976a3dfec793010b4c8594221e13f3
najtwisak-pc\Jakub <jlerl@atlas.cz>
2018-10-20 18:45:34 +0200

    zmena implementace serializeru z xml na json

--- mobile/TestApp/TestApp/Services/SerializerProvider.cs
+++ mobile/TestApp/TestApp/Services/SerializerProvider.cs
@@ -1,34 +1,24 @@
-﻿using System.IO;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Xml.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace GayTimer.Services
 {
     public interface ISerializerProvider
     {
-        void Serialize<TObject>(TObject obj, Stream path);
+        string Serialize<TObject>(TObject obj);
 
-        TObject Deserialize<TObject>(Stream str);
+        TObject Deserialize<TObject>(string str);
     }
 
     public class SerializerProvider : ISerializerProvider
     {
-        public TObject Deserialize<TObject>(Stream str)
+        public TObject Deserialize<TObject>(string value)
         {
-            var serializer = new XmlSerializer(typeof(TObject));
-
-            var deserialized = (TObject)serializer.Deserialize(str);
-
-            return deserialized;
+            return JsonConvert.DeserializeObject<TObject>(value);
         }
 
-        public void Serialize<TObject>(TObject obj, Stream str)
+        public string Serialize<TObject>(TObject obj)
         {
-            var serializer = new XmlSerializer(typeof(TObject));
-
-            serializer.Serialize(str, obj);
+            return JsonConvert.SerializeObject(obj);
         }
     }
 }
