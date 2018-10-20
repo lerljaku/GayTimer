3312818a338928a380f1dfba533caa3e6f0e8e06
najtwisak-pc\Jakub <jlerl@atlas.cz>
2018-10-20 18:44:35 +0200

    add Testovaci assembly, test na serializaci

---
+++ mobile/GayTimes.Tests/SerizalizerFixture.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using GayTimer.Entities;
+using GayTimer.Services;
+using NUnit.Framework;
+
+namespace GayTimes.Tests
+{
+    [TestFixture]
+    public class SerizalizerFixture
+    {
+        private SerializerProvider SUT;
+        
+        [SetUp]
+        public void Init()
+        {
+            SUT = new SerializerProvider();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var data = new Gay()
+            {
+                Id = 1,
+                Created = new DateTime(2000,01,01),
+                Nick = "OttoGej",
+                PasswordHash = "456sdfwer",
+                PasswordSalt = "45sdafdasfa86erw32sadf6asdf",
+            };
+
+            var serData = SUT.Serialize(data);
+
+            var user = SUT.Deserialize<Gay>(serData);
+
+            Assert.AreEqual(data.Id, user.Id);
+            Assert.AreEqual(data.Created, user.Created);
+            Assert.AreEqual(data.PasswordHash, user.PasswordHash);
+            Assert.AreEqual(data.PasswordSalt, user.PasswordSalt);
+            Assert.AreEqual(data.Nick, user.Nick);
+        }
+    }
+}
---
+++ mobile/TestApp/TestApp/Services/ErrorService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GayTimer.Services
+{
+    public interface IErrorService
+    {
+        void LogError(string message);
+        void LogWarn(string message);
+        void LogInfo(string message);
+        void LogDebug(string message);
+
+        void LogError(Exception ex);
+        void LogWarn(Exception ex);
+        void LogInfo(Exception ex);
+        void LogDebug(Exception ex);
+    }
+
+    public class ErrorService : IErrorService
+    {
+        public ObservableCollection<string> Errors { get; } = new ObservableCollection<string>();
+
+        public void LogError(string message)
+        {
+            Errors.Add(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            Errors.Add(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Errors.Add(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            Errors.Add(message);
+        }
+
+        public void LogError(Exception ex)
+        {
+            LogError(ex.Message);
+        }
+
+        public void LogWarn(Exception ex)
+        {
+            LogWarn(ex.Message);
+        }
+
+        public void LogInfo(Exception ex)
+        {
+            LogInfo(ex.Message);
+        }
+
+        public void LogDebug(Exception ex)
+        {
+            LogDebug(ex.Message);
+        }
+    }
+}
